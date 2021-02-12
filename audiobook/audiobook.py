5b4a43b1a2309ce79f286e1f3b5c1e1051337095
Aman Pandit <60512626+amanpanditap@users.noreply.github.com>
2021-02-12 19:36:45 +0530

    Update audiobook.py

--- audiobook/audiobook.py
+++ audiobook/audiobook.py
@@ -2,16 +2,16 @@
 # run the install code in Terminal first
 import pyttsx3   # install pip pyttsx3
 import PyPDF2    # install pip pyPDF2
-book = open('book.pdf', 'rb')    # put the pdf name instead of 'A_Christmas_Carol_NT.pdf'
+book = open('book.pdf', 'rb')    # put the pdf name instead of 'book.pdf'
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
 for page_num in range(pages):
-    page = pdfReader.getPage(page_num)   # put page no instead of 3
+    page = pdfReader.getPage(page_num)   # put page no 
     voices = speaker.getProperty('voices')
     speaker.setProperty('voice', voices[0].id)  # put 0 or 1 to change voice 0:male 1:female
-    speaker.setProperty('rate', 180)  # put the letters per minute rate in place og 150
+    speaker.setProperty('rate', 150)  # put the letters per minute rate in place og 150
     speaker.say(page.extractText())
     speaker.runAndWait()
 speaker.stop()
